db302bf8e1e7456032870f4fe1236b28e4444c20
chetandd <chetan.dhandapani@emids.com>
2015-10-27 11:53:00 +0530

    excel parser added

--- server.js
+++ server.js
@@ -1,48 +1,52 @@
-var express         =    require("express");
-var multer          =    require('multer');
-var app             =    express();
-var filePath        =    'where you store the file';
-var upload      =   multer({ dest: filePath});
+var express = require('express')
+var multer = require('multer')
+var app = express()
+var filePath = 'uploads'
+var upload = multer({ dest: filePath})
+var xlsx = require('node-xlsx')
 
 app.use(multer({ dest: filePath,
-    rename: function (fieldname, filename) {
-        return filename+Date.now();
-    },
-    onFileUploadStart: function (file) {
-        console.log(file.originalname + ' is starting ...');
-    },
-    onFileUploadComplete: function (file) {
-        console.log(file.fieldname + ' uploaded to  ' + file.path)
+  rename: function (fieldname, filename) {
+    return filename + Date.now()
+  },
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + ' is starting ...')
+  },
+  onFileUploadComplete: function (file) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path)
+  }
+}))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/upload', function (req, res) {
+  res.sendFile(__dirname + '/public/app/index.html')
+})
+
+app.post('/api/photo', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end('Error uploading file.')
     }
-}));
+    res.end('File is uploaded')
+  })
+})
 
-app.get('/',function(req,res){
-      res.sendFile(__dirname + "/index.html");
-});
+app.post('/api/photoFile', function (req, res) {
+  // console.log('--->', req.files.file)
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end('Error uploading file.')
+    }
+    var obj = xlsx.parse(__dirname + '\\' + req.files.file.path) // parses a file
 
-app.get('/upload',function(req,res){
-      res.sendFile(__dirname + "/public/app/index.html");
-});
+    // res.end('parsed data')
+    res.send(obj)
+  })
+})
 
-app.post('/api/photo',function(req,res){
-    upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
-});
-
-
-app.post('/api/photoFile',function(req,res){
-    upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
-});
-
-app.listen(3000,function(){
-    console.log("Working on port 3000");
-});
+app.listen(3000, function () {
+  console.log('Working on port 3000')
+})
